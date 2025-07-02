77b82c5e4eb3c5845c8036d62f77b530a1909c94
mmn <manoj.mn@amadeus.com>
2025-07-02 15:02:23 +0530

    test change

--- src/main/java/org/example/practice/api/WelcomeController.java
+++ src/main/java/org/example/practice/api/WelcomeController.java
@@ -18,8 +18,9 @@ public class WelcomeController {
     public String welcome(Model model) throws UnknownHostException {
         String podIp = InetAddress.getLocalHost().getHostAddress();
         if(podName.equals("unknown")) podName = InetAddress.getLocalHost().getHostName();
-        model.addAttribute("podName", podName);
-        model.addAttribute("podIp", podIp);
+        model.addAttribute("podName MJ: ", podName);
+        model.addAttribute("podIp: ", podIp);
+
         return "welcome";
     }
 }
